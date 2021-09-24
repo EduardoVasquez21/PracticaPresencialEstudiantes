81cdd4c7a8e1c7f61230a4857332a875f5000789
EduardoVasquez21 <78975314+EduardoVasquez21@users.noreply.github.com>
2021-09-23 22:15:11 -0600

    PracticaUsuarios

---
+++ EstudiantesPresencial16sep/Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using EstudiantesPresencial16sep.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstudiantesPresencial16sep.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+            }
+            else
+            {
+                //return View("Index", usuario);
+                return Redirect("Usuario/Error");
+            }
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EstudiantesPresencial16sep/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstudiantesPresencial16sep.Models
+{
+    public class Usuario
+    {
+        [Display(Name = "Nombre Usuario")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public String NombreUsuario { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public int Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public String Sexo { get; set; }
+
+        [Display(Name = "Tipo de educacion")]
+        [Required(ErrorMessage = "Error, este campo es requerido")]
+        public String Tipodeeducacion { get; set; }
+    }
+}
